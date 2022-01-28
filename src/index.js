3579a7b1734eccea7d5448e585acf50ebc46fc2d
Yadhunandana Gowda B S <65999096+YadhunandanGowda@users.noreply.github.com>
2022-01-28 22:28:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let app = express();
+let portNumber = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(portNumber, (err) => {
+  if (err) console.log("Error in server setup");
+  console.log("Server listening on Port:", portNumber);
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function (req, res) {
+  let respo = {
+    developer: "Yadhunandana Gowda B S",
+    from: "Mysuru",
+  };
+  res.send(respo);
+});
+
+app.post("/post", (req, res) => {
+  console.log(req.body.name);
+  res.send([...req.body.name].reverse().join(""));
+});
